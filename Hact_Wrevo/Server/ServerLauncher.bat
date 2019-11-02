5b55b22c5983ff17c32cf77b5e76bbfd8daed020
SilkooDarze <tsaotsaotsao51510@gmail.com>
2019-11-02 23:53:36 +0800

    Hide some secret files

--- Hact_Wrevo/Server/ServerLauncher.bat
+++
@@ -1,5 +0,0 @@
-start Hact_Wrevo_LoginServerV.01.py
-start Hact_Wrevo_mainChatDistributionServerV.01.py
-start Hact_Wrevo_mainChatServerV.01.py
-start Hact_Wrevo_updateServerV.02.py
-start Hact_Wrevo_versCheckServerV.01.py
