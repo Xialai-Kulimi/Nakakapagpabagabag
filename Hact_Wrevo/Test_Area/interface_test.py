b9be938dc1ec1df474f8b44d42ccbb759258cd5b
Kulimi <tsaotsaotsao51510@gmail.com>
2019-12-17 12:50:01 +0800

    Create interface_test.py

---
+++ Hact_Wrevo/Test_Area/interface_test.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+import os
+
+root = tk.Tk()
+top_frame = tk.Frame(root)
+top_frame.pack()
+root.overrideredirect(True)
+root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
+
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+print(screen_height, screen_width)
+
+root.mainloop()
+
+while True:
+    pass
