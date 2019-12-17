d5ad4aff7bf9e154ab6d620a01e496c9a92742ca
Kulimi <tsaotsaotsao51510@gmail.com>
2019-12-17 13:00:53 +0800

    update

--- Hact_Wrevo/Test_Area/interface_test.py
+++ Hact_Wrevo/Test_Area/interface_test.py
@@ -2,10 +2,12 @@ import tkinter as tk
 import os
 
 root = tk.Tk()
+root.title('Hact_Wrevo')
+root.geometry('800x600')
 # top_frame = tk.Frame(root)
 # top_frame.pack()
-root.overrideredirect(True)
-root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
+# root.overrideredirect(True)
+# root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
 
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
---
+++ Hact_Wrevo/Test_Area/test03.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.geometry('0x1+0+0')
+root.resizable(False, False)
+root.update_idletasks()
+root.overrideredirect(True)
+root.mainloop()
