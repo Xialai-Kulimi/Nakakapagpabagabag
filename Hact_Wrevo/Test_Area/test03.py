0d97db1dc3b1512bcd8ecca87e0b3422c91ad7d6
Kulimi <tsaotsaotsao51510@gmail.com>
2019-12-19 16:12:53 +0800

    Update test03.py

--- Hact_Wrevo/Test_Area/test03.py
+++ Hact_Wrevo/Test_Area/test03.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 
 root = tk.Tk()
-root.geometry('0x1+0+0')
+root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
 root.resizable(False, False)
 root.update_idletasks()
 root.overrideredirect(True)
