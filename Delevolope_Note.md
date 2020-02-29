7813f80ec84a027cf1bc945d6bd36db4cfc8caf5
Kulimi <tsaotsaotsao51510@gmail.com>
2020-02-29 17:17:20 +0800

    update

--- Delevolope_Note.md
+++ Delevolope_Note.md
@@ -1,5 +1,9 @@
-Delovoper Notes
+Developer Notes
 ===
 Stores the link to common issue.
 
 ## Tkinter
+
+1. [Basic tutorial_如何使用 Python Tkinter 製作 GUI 應用程式入門教學](https://blog.techbridge.cc/2019/09/21/how-to-use-python-tkinter-to-make-gui-app-tutorial/)
+2. 
+3.
--- Hact_Wrevo/Test_Area/interface_test.py
+++ Hact_Wrevo/Test_Area/interface_test.py
@@ -1,20 +1,33 @@
 import tkinter as tk
-import os
+# 建立主視窗和 Frame（把元件變成群組的容器）
+window = tk.Tk()
+top_frame = tk.Frame(window)
 
-root = tk.Tk()
-root.title('Hact_Wrevo')
-root.geometry('800x600')
-# top_frame = tk.Frame(root)
-# top_frame.pack()
-# root.overrideredirect(True)
-# root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
+# 將元件分為 top/bottom 兩群並加入主視窗
+top_frame.pack()
+bottom_frame = tk.Frame(window)
+bottom_frame.pack(side=tk.BOTTOM)
 
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
+# 建立事件處理函式（event handler），透過元件 command 參數存取
+def echo_hello():
+    print('hello world :)')
 
-print(screen_height, screen_width)
+# 以下為 top 群組
+left_button = tk.Button(top_frame, text='Red', fg='red')
+# 讓系統自動擺放元件，預設為由上而下（靠左）
+left_button.pack(side=tk.LEFT)
 
-root.mainloop()
+middle_button = tk.Button(top_frame, text='Green', fg='green')
+middle_button.pack(side=tk.LEFT)
 
-while True:
-    pass
+right_button = tk.Button(top_frame, text='Blue', fg='blue')
+right_button.pack(side=tk.LEFT)
+
+# 以下為 bottom 群組
+# bottom_button 綁定 echo_hello 事件處理，點擊該按鈕會印出 hello world :)
+bottom_button = tk.Button(bottom_frame, text='Black', fg='black', command=echo_hello)
+# 讓系統自動擺放元件（靠下方）
+bottom_button.pack(side=tk.BOTTOM)
+
+# 運行主程式
+window.mainloop()
