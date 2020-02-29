834c69f491a8241f8854ae1ed6179f1075ad71d6
Kulimi <tsaotsaotsao51510@gmail.com>
2020-02-29 17:54:16 +0800

    Update Hact_WrevoV.3.py

--- Hact_Wrevo/Client/Hact_WrevoV.3.py
+++ Hact_Wrevo/Client/Hact_WrevoV.3.py
@@ -2,6 +2,7 @@ import tkinter as tk
 
 window = tk.Tk()  # Creating the root window.
 window.title('Hact Wrevo')  # Set the title.
+window.
 login_GUI = tk.Frame(window)  # Create the group of login GUI.
 login_GUI.pack(side=tk.TOP)
 
