e37459ca251b6aa757c88085906a9ff567cfb5f3
Kulimi <tsaotsaotsao51510@gmail.com>
2020-02-29 17:38:07 +0800

    Update Hact_WrevoV.3.py

--- Hact_Wrevo/Client/Hact_WrevoV.3.py
+++ Hact_Wrevo/Client/Hact_WrevoV.3.py
@@ -1,9 +1,10 @@
 import tkinter as tk
 
-window = tk.Tk()
-window.title('Hact Wrevo')
-login_GUI = tk.Frame(window)
-welcome = tk.Label(login_GUI, text="Hello Tk!", width="30", height="5")
-welcome.pack()
+window = tk.Tk()  # Creating the root window.
+window.title('Hact Wrevo')  # Set the title.
+login_GUI = tk.Frame(window)  # Create the group of login GUI.
+
+welcome = tk.Label(login_GUI, text="Hello Tk!")
+welcome.pack(side=tk.TOP)
 # root.tk.call('wm', 'iconphoto', root.w, tk.PhotoImage(file='./asset/new.png'))
 window.mainloop()
