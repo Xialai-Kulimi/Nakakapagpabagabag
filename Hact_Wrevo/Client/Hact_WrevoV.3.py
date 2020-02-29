25abc2aa576adc69cccb384ec1dfe180e63f180b
Kulimi <tsaotsaotsao51510@gmail.com>
2020-02-29 17:59:33 +0800

    Update Hact_WrevoV.3.py

--- Hact_Wrevo/Client/Hact_WrevoV.3.py
+++ Hact_Wrevo/Client/Hact_WrevoV.3.py
@@ -4,9 +4,9 @@ window = tk.Tk()  # Creating the root window.
 window.title('Hact Wrevo')  # Set the title.
 window['bg'] = 'black'
 login_GUI = tk.Frame(window)  # Create the group of login GUI.
-login_GUI.pack(side=tk.TOP)
+login_GUI.pack()
 
-welcome = tk.Label(login_GUI, text="Hello Tk!", fg='white', bg='black')
+welcome = tk.Label(login_GUI, text="Hello Tk!", fg='white', bg='black', font='helvetica 24')
 welcome.pack()
 
 # root.tk.call('wm', 'iconphoto', root.w, tk.PhotoImage(file='./asset/new.png'))
