4d2bf166410313a6b8b7abce6581752b9c79113d
SilkooDarze <tsaotsaotsao51510@gmail.com>
2020-03-01 16:08:18 +0800

    update

--- venv/Scripts/easy_install-script.py
+++ venv/Scripts/easy_install-script.py
@@ -1,4 +1,4 @@
-#!C:\Users\Kulimi\PycharmProjects\burg\venv\Scripts\python.exe
+#!D:\Nakakabagpabagabag\Hact_Wrevo\venv\Scripts\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==40.8.0','console_scripts','easy_install'
 __requires__ = 'setuptools==40.8.0'
 import re
